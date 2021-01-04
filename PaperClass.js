96641f6f9fca575bdc8b4cf498b3fec8beeffb53
I-Am-Joe <72526251+I-Am-Joe@users.noreply.github.com>
2021-01-04 16:07:47 -0500

    Add files via upload

---
+++ PaperClass.js
@@ -0,0 +1,27 @@
+class PaperClass{
+	constructor(x,y,diameter){
+		var options = {
+			isStatic:false,
+			'restitution':0.3,
+			'friction':0.5,
+			'density':1.2,
+		}
+		this.body=Bodies.circle(x,y,diameter,options);
+		this.diameter=diameter;
+		this.image = loadImage("sprites/paper.png");
+  
+		World.add(world,this.body);
+	}
+  
+	display(){
+		var pos=this.body.position;
+		var angle=this.body.angle;
+		push();
+		translate(pos.x,pos.y);
+		rotate(angle);
+		ellipseMode(RADIUS);
+		fill("white");
+		ellipse(0,0,this.diameter);       
+		pop();
+	}
+  };
